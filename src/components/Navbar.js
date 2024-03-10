9532ee2d703035b5ed94ca28fd5567bd47030c1b
Bruno <brunohgrau99@gmail.com>
2024-03-09 22:36:25 -0300

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,6 +9,7 @@ import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
+import { mainNavbarItems } from "./consts/navbarItems";
 
 const Navbar = () => {
   const drawerWidth = 200;
@@ -29,26 +30,11 @@ const Navbar = () => {
         <Toolbar />
         <Divider />
         <List>
-          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-            <ListItem key={text} disablePadding>
+          {mainNavbarItems.map((text, index) => (
+            <ListItem key={text.id} disablePadding>
               <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {["All mail", "Trash", "Spam"].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
+                <ListItemIcon>{text.icon}</ListItemIcon>
+                <ListItemText primary={text.label} />
               </ListItemButton>
             </ListItem>
           ))}
