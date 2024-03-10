700634f56c3944b6c785dae90f3e00ab3804baa6
Bruno <brunohgrau99@gmail.com>
2024-03-09 23:22:40 -0300

    Add Customization

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,34 +7,21 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
 import { mainNavbarItems } from "./consts/navbarItems";
+import { navbarStyles } from "./styles.js";
 
 const Navbar = () => {
-  const drawerWidth = 200;
   return (
     <>
-      <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          "& .MuiDrawer-paper": {
-            width: drawerWidth,
-            boxSizing: "border-box",
-          },
-        }}
-        variant="permanent"
-        anchor="left"
-      >
+      <Drawer sx={navbarStyles.drawer} variant="permanent" anchor="left">
         <Toolbar />
         <Divider />
         <List>
           {mainNavbarItems.map((text, index) => (
             <ListItem key={text.id} disablePadding>
               <ListItemButton>
-                <ListItemIcon>{text.icon}</ListItemIcon>
-                <ListItemText primary={text.label} />
+                <ListItemIcon sx={navbarStyles.icons}>{text.icon}</ListItemIcon>
+                <ListItemText sx={navbarStyles.text} primary={text.label} />
               </ListItemButton>
             </ListItem>
           ))}
