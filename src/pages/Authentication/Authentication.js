0c12a9f91122ecb5dc46fd8884cbc646cc353ac1
Bruno <brunohgrau99@gmail.com>
2024-03-11 15:20:24 -0300

    Searchbar

--- src/pages/Authentication/Authentication.js
+++ src/pages/Authentication/Authentication.js
@@ -1,9 +1,66 @@
 import React from "react";
-import Grid from "@mui/material/Grid";
-import Header from "../../components/Header/Header";
+import RefreshIcon from "@mui/icons-material/Refresh";
+import BasicCard from "../../components/common/BasicCard/BasicCard";
+import SearchBar from "../../components/common/SearchBar/SearchBar";
+import CommonButton from "../../components/common/CommonButton/CommonButton";
+import { Box, Typography } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import GridWrapper from "../../components/common/GridWrapper/GridWrapper";
+import { cardHeaderStyles } from "./styles";
 
 const Authentication = () => {
-  return <Grid item xs={8}></Grid>;
+  const getHeader = () => {
+    const handleChange = (value) => {
+      console.log(value);
+    };
+
+    const addUser = () => {
+      console.log("click");
+    };
+
+    return (
+      <Box sx={cardHeaderStyles.wrapper}>
+        <SearchBar
+          placeholder="Search by email address"
+          onChange={(event) => handleChange(event.target.value)}
+          searchBarWidth="720px"
+        />
+        <Box>
+          <CommonButton
+            variant="contained"
+            onClick={addUser}
+            size="large"
+            sx={cardHeaderStyles.addUserButton}
+          >
+            Add User
+          </CommonButton>
+
+          <IconButton>
+            <RefreshIcon />
+          </IconButton>
+        </Box>
+      </Box>
+    );
+  };
+  const getContent = () => {
+    return (
+      <Typography
+        align="center"
+        sx={{
+          margin: "40px 10px",
+          color: "rgba(0, 0, 0, 0.6)",
+          fontSize: "1.3rem",
+        }}
+      >
+        No users for this project yet
+      </Typography>
+    );
+  };
+  return (
+    <GridWrapper>
+      <BasicCard header={getHeader()} content={getContent()} />
+    </GridWrapper>
+  );
 };
 
 export default Authentication;
